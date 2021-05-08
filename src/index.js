c537d2ac68447d3f9211c60a396dc441d11f1170
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-08 20:09:23 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fname = "Hemanth";
+const lname = "Reddy";
+
+var date = new Date();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {fname + " " + lname}</p>
+    <p>Copyright {date.getFullYear()}</p>
+  </div>,
+  document.getElementById("root")
+);
